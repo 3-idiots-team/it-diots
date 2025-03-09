e4ad11be32dd32b16da375cf44b941b526ad8f87
p-acid <qkrtkstjd01@gmail.com>
2025-03-09 10:42:07 +0900

    feat: sidebar 디자인 변경

---
+++ packages/shared/src/widgets/layouts/main-layout/sidebar/index.tsx
@@ -0,0 +1,23 @@
+import {
+  Sidebar as ShadcnSidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+} from '@it-diots/shared/ui';
+
+import { RouteGroups } from './route-groups';
+import { SidebarUser } from './sidebar-user';
+
+export const Sidebar = () => {
+  return (
+    <ShadcnSidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarUser />
+      </SidebarHeader>
+      <SidebarContent>
+        <RouteGroups />
+      </SidebarContent>
+      <SidebarFooter />
+    </ShadcnSidebar>
+  );
+};
