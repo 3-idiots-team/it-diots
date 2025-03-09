cdef0688c1c2fb0ec990191f58be5269ec2de1c5
이치호 <minepanorama@naver.com>
2025-03-10 02:24:36 +0900

    chore: tag 페이지 구현

--- apps/web/src/pages/tag/ui/index.tsx
+++ apps/web/src/pages/tag/ui/index.tsx
@@ -1,9 +1,23 @@
-import { PageLoading } from '@/shared/ui/page-loading';
+import { Separator } from '@it-diots/shared/ui';
+
+import { AllTags } from '@/widgets/all-tags/ui';
+import { TagListForm } from '@/widgets/tag-list-form/ui';
 
 export function TagPage() {
   return (
-    <div className="p-4 w-full">
-      <PageLoading />
+    <div className="w-full flex flex-col gap-6 p-4">
+      <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-6 items-start">
+        <TagListForm title="🔥 추천 태그" />
+        <TagListForm title="🚀 인기 태그" />
+        <TagListForm title="🔎 최근 추가된 태그" />
+      </div>
+
+      <div>
+        <p className="text-xl font-bold">모든 태그</p>
+        <Separator className="my-4" />
+
+        <AllTags />
+      </div>
     </div>
   );
 }
---
+++ apps/web/src/widgets/tag-list-form/index.ts
@@ -0,0 +1,1 @@
+export { TagListForm } from './ui';
