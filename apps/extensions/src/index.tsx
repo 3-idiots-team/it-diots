e23f8c2dd167ea0248df2964411ab7777d296192
이치호 <minepanorama@naver.com>
2025-03-11 00:59:35 +0900

    fix: extension 에러 수정

--- apps/extensions/src/index.tsx
+++ apps/extensions/src/index.tsx
@@ -7,12 +7,28 @@ import App from './App';
 import '@it-diots/shared/globals.css';
 import { Providers } from './providers';
 
+const VIRTUAL_USER = {
+  thumbnail:
+    'https://images.unsplash.com/photo-1552058544-f2b08422138a?q=80&w=1398&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  nickname: 'it-diots',
+  email: 'itdiots@gmail.com',
+};
+
+/** FIXME: 로그아웃, 유저정보 추가 필요 */
 const container = document.getElementById('root');
 if (!container) throw new Error('Failed to find the root element');
 const root = createRoot(container);
 root.render(
   <Providers>
-    <MainLayout>
+    <MainLayout
+      onSignOut={() => alert('로그아웃')}
+      userInfo={{
+        avatar_url: VIRTUAL_USER.thumbnail,
+        email: VIRTUAL_USER.email,
+        id: '',
+        username: VIRTUAL_USER.nickname,
+      }}
+    >
       <App />
     </MainLayout>
   </Providers>
