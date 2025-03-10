e245b86bd579cbbd2521911e664dc265c9498264
이치호 <minepanorama@naver.com>
2025-03-11 00:38:58 +0900

    fix: 줄바꿈 추가

--- packages/shared/src/widgets/layouts/main-layout/index.tsx
+++ packages/shared/src/widgets/layouts/main-layout/index.tsx
@@ -14,6 +14,7 @@ export function MainLayout({
   return (
     <SidebarProvider>
       <Sidebar onSignOut={onSignOut} userInfo={userInfo} />
+
       <SidebarInset>
         <Header />
         {children}
