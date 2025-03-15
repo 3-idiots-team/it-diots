51028b27f7ed20f25d84e57e19d74ebee9d7bc4f
이치호 <minepanorama@naver.com>
2025-03-15 23:38:50 +0900

    fix: tag 페이지 관련 코드 삭제

--- apps/web/src/pages/tag/ui/index.tsx
+++ apps/web/src/pages/tag/ui/index.tsx
@@ -1,23 +1,3 @@
-import { Separator } from '@it-diots/shared/ui';
-
-import { AllTags } from '@/features/all-tags';
-import { TagListForm } from '@/widgets/tag-list-form/ui';
-
 export function TagPage() {
-  return (
-    <div className="w-full flex flex-col gap-6 p-4">
-      <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-6 items-start">
-        <TagListForm title="🔥 추천 태그" />
-        <TagListForm title="🚀 인기 태그" />
-        <TagListForm title="🔎 최근 추가된 태그" />
-      </div>
-
-      <div>
-        <p className="text-xl font-bold">모든 태그</p>
-        <Separator className="my-4" />
-
-        <AllTags />
-      </div>
-    </div>
-  );
+  return <div>TagPage</div>;
 }
